1de0191923bfb041cc84e348fdb447ea0d125d7d
WillianJrBastos <wjbastos1@gmail.com>
2026-09-12 11:35:00 -0300

    ContatoEmergencia configurado

--- src/main/java/com/zelo/entity/ContatoEmergencia.java
+++ src/main/java/com/zelo/entity/ContatoEmergencia.java
@@ -1,4 +1,32 @@
 package com.zelo.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "contato_emergencia")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ContatoEmergencia {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String telefone;
+
+    private String parentesco;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "usuario_id", nullable = false)
+    @JsonIgnore
+    private Usuario usuario;
 }
