a611bf3a9e68091de120a62a4ffb1d5d09b39872
WillianJrBastos <wjbastos1@gmail.com>
2026-09-12 11:53:48 -0300

    RegistroMedicacao configurado

--- src/main/java/com/zelo/entity/RegistroMedicacao.java
+++ src/main/java/com/zelo/entity/RegistroMedicacao.java
@@ -1,4 +1,35 @@
 package com.zelo.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "registro_medicacao")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class RegistroMedicacao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "data_hora_agendada", nullable = false)
+    private LocalDateTime dataHoraAgendada;
+
+    @Column(name = "data_hora_acao")
+    private LocalDateTime dataHoraAcao;
+
+    @Column(nullable = false)
+    private String status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "alarme_id", nullable = false)
+    @JsonIgnore
+    private Alarme alarme;
 }
