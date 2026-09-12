a8f2cb30b4516d7388ce8dafe087a9dd9014f79e
WillianJrBastos <wjbastos1@gmail.com>
2026-09-12 14:15:26 -0300

    Finalizado repository com operações para listar os contatos de emergencia de um usuario

--- src/main/java/com/zelo/repository/ContatoEmergenciaRepository.java
+++ src/main/java/com/zelo/repository/ContatoEmergenciaRepository.java
@@ -1,4 +1,11 @@
 package com.zelo.repository;
 
-public interface ContatoEmergenciaRepository {
+import com.zelo.entity.ContatoEmergencia;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ContatoEmergenciaRepository extends JpaRepository<ContatoEmergencia, Long> {
+
+    List<ContatoEmergencia> findByUsuarioId(Long usuarioId);
 }
