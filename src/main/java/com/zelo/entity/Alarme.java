2fb3d06bd852c2cda5d74b0c6aa6f0659c2828fc
WillianJrBastos <wjbastos1@gmail.com>
2026-09-12 11:25:42 -0300

    Alarme configurado

--- src/main/java/com/zelo/entity/Alarme.java
+++ src/main/java/com/zelo/entity/Alarme.java
@@ -1,4 +1,35 @@
 package com.zelo.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "alarme")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Alarme {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalTime hora;
+
+    @Column(nullable = false)
+    private Boolean ativo = true;
+
+    @Column(name = "adiar_minutos")
+    private Integer adiarMinutos = 10;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "medicamento_id", nullable = false)
+    @JsonIgnore
+    private Medicamento medicamento;
 }
