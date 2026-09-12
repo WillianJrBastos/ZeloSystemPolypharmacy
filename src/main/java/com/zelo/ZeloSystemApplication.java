bb691eacc1aa59e1ba0860a3f15bb27db202af1c
WillianJrBastos <wjbastos1@gmail.com>
2026-09-12 10:22:11 -0300

    Criado o arquivo Spring Boot para o desenvolvimento do projeto

---
+++ src/main/java/com/zelo/ZeloSystemApplication.java
@@ -0,0 +1,13 @@
+package com.zelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeloSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeloSystemApplication.class, args);
+	}
+
+}
