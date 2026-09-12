aa0b2611daa791b40efe5b3ecca68b3f3d2a9b94
WillianJrBastos <wjbastos1@gmail.com>
2026-09-12 10:49:10 -0300

    Iniciando a configuração da classe Usuario

--- src/main/java/com/zelo/entity/Usuario.java
+++ src/main/java/com/zelo/entity/Usuario.java
@@ -1,4 +1,20 @@
 package com.zelo.entity;
 
+import java.time.LocalDate;
+
 public class Usuario {
+
+    private long id;
+    private String nome;
+    private LocalDate dataNascimento;
+    private String email;
+    private String senha;
+    private String tipoUsuario;
+    private String telefone;
+    private String fotoUrl;
+    private String tipoSanguineo;
+    private String alergias;
+    private String condicaoSaude;
+    private String observacoesImportantes;
+    private Usuario cuidador;
 }
