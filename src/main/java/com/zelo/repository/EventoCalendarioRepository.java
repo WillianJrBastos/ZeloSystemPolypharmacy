0bd5e8fdf7c471fff779e6b4e9bead9041ff37c8
WillianJrBastos <wjbastos1@gmail.com>
2026-09-12 14:31:42 -0300

    Finalizado repository com operações para listar os eventos de um usuario

--- src/main/java/com/zelo/repository/EventoCalendarioRepository.java
+++ src/main/java/com/zelo/repository/EventoCalendarioRepository.java
@@ -1,4 +1,16 @@
 package com.zelo.repository;
 
-public interface EventoCalendarioRepository {
+import com.zelo.entity.EventoCalendario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface EventoCalendarioRepository extends JpaRepository<EventoCalendario, Long> {
+
+    List<EventoCalendario> findByUsuarioId(Long usuarioId);
+
+    List<EventoCalendario> findByUsuarioIdAndDataHoraBetween(Long usuarioId, LocalDateTime inicio, LocalDateTime fim);
+
+    boolean existsByUsuarioIdAndTipoAndTitulo(Long usuarioId, String tipo, String titulo);
 }
