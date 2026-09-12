766aeda0562395ef681259842e828b13975566d3
WillianJrBastos <wjbastos1@gmail.com>
2026-09-12 14:41:37 -0300

    Finalizado repository com operações para listar os registros de medicação

--- src/main/java/com/zelo/repository/RegistroMedicacaoRepository.java
+++ src/main/java/com/zelo/repository/RegistroMedicacaoRepository.java
@@ -1,4 +1,16 @@
 package com.zelo.repository;
 
-public interface RegistroMedicacaoRepository {
+import com.zelo.entity.RegistroMedicacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface RegistroMedicacaoRepository extends JpaRepository<RegistroMedicacao, Long> {
+
+    List<RegistroMedicacao> findByAlarmeId(Long alarmeId);
+
+    List<RegistroMedicacao> findByAlarmeMedicamentoUsuarioIdOrderByDataHoraAgendadaDesc(Long usuarioId);
+
+    List<RegistroMedicacao> findByAlarmeMedicamentoUsuarioIdAndDataHoraAgendadaBetween(Long usuarioId, LocalDateTime inicio, LocalDateTime fim);
 }
