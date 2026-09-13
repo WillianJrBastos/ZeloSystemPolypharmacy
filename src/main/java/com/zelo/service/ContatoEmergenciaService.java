a575ac8eb4fb631a6d22c1c99fbecfd6065e8a30
WillianJrBastos <wjbastos1@gmail.com>
2026-09-13 14:09:00 -0300

    Criado metodos para cadatrar contatos de emergencia

--- src/main/java/com/zelo/service/ContatoEmergenciaService.java
+++ src/main/java/com/zelo/service/ContatoEmergenciaService.java
@@ -1,4 +1,28 @@
 package com.zelo.service;
 
+import com.zelo.dto.ContatoEmergenciaDTO;
+import com.zelo.entity.ContatoEmergencia;
+import com.zelo.entity.Usuario;
+import com.zelo.repository.ContatoEmergenciaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ContatoEmergenciaService {
+
+    @Autowired
+    private ContatoEmergenciaRepository contatoEmergenciaRepository;
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    public ContatoEmergencia cadastrar(Long usuarioId, ContatoEmergenciaDTO dto) {
+        Usuario usuario = usuarioService.buscarPorId(usuarioId);
+        ContatoEmergencia contatoEmergencia = new ContatoEmergencia();
+        contatoEmergencia.setNome(dto.getNome());
+        contatoEmergencia.setTelefone(dto.getTelefone());
+        contatoEmergencia.setParentesco(dto.getParentesco());
+        contatoEmergencia.setUsuario(usuario);
+        return contatoEmergenciaRepository.save(contatoEmergencia);
+    }
 }
