54137b1f38ca912354b431351515667b26731858
WillianJrBastos <wjbastos1@gmail.com>
2026-09-13 10:44:45 -0300

    Criado metodos para facilitar a criação das notificações

--- src/main/java/com/zelo/service/NotificacaoService.java
+++ src/main/java/com/zelo/service/NotificacaoService.java
@@ -44,4 +44,24 @@ public class NotificacaoService {
         notificacao.setLida(true);
         return notificacaoRepository.save(notificacao);
     }
+
+    public void notificarEstoqueBaixo(Usuario usuario, String nomeMedicamento, int quantidadeRestante) {
+        String titulo = "Estoque Baixo";
+        String descricao = "Restam " + quantidadeRestante + " comprimidos de " + nomeMedicamento;
+        boolean jaExiste = notificacaoRepository.existsByUsuarioIdAndTipoAndTituloAndLidaFalse(usuario.getId(), "ESTOQUE_BAIXO", titulo);
+
+        if (!jaExiste) {
+            criar(usuario, titulo, descricao, "ESTOQUE_BAIXO", LocalDateTime.now());
+        }
+    }
+
+    public void notificarEvento(Usuario usuario, String tituloEvento, String tipoEvento, LocalDateTime dataHoraEvento) {
+        String descricao = dataHoraEvento.toLocalTime() + " - " + tituloEvento;
+        criar(usuario, tituloEvento.equals("EXAME") ? "Exame" : "Consulta", descricao, tipoEvento, dataHoraEvento);
+    }
+
+    public void notificarHoraMedicacao(Usuario usuario, String nomeMedicamento, LocalDateTime dataHoraAlarme) {
+        String descricao = String.format("%02d:%02d", dataHoraAlarme.getHour(), dataHoraAlarme.getMinute()) + " - " +nomeMedicamento;
+        criar(usuario, "Hora da medicação", descricao, "MEDICACAO", dataHoraAlarme);
+    }
 }
