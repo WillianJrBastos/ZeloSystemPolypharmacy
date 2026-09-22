d9070d7161c0618455e3ab3d82d2eb829cb52019
WillianJrBastos <wjbastos1@gmail.com>
2026-09-22 08:27:29 -0300

    Controller de autenticação com login e cadastro finalizado

--- src/main/java/com/zelo/controller/AuthController.java
+++ src/main/java/com/zelo/controller/AuthController.java
@@ -1,4 +1,40 @@
 package com.zelo.controller;
 
+import com.zelo.dto.CadastroUsuarioDTO;
+import com.zelo.dto.LoginDTO;
+import com.zelo.entity.Usuario;
+import com.zelo.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @PostMapping("/cadastro")
+    public ResponseEntity<Usuario> cadastrar(@RequestBody CadastroUsuarioDTO dto) {
+        Usuario novoUsuario = usuarioService.cadastrar(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novoUsuario);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginDTO dto) {
+        Optional<Usuario> usuario = usuarioService.login(dto);
+
+        if (usuario.isPresent()) {
+            return ResponseEntity.ok(usuario.get());
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Email ou senha inválidos");
+        }
+    }
 }
